bca2a109461ae0eb2e7befba2d145da2a95eb4c7
Yu-Pang.Chen <Ted.Chen@china.nttdata.com>
2024-05-16 10:24:21 +0800

    :bug: fix ui blocking while fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
 'use client';
 
 import { type CoreMessage } from 'ai';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { continueConversation } from './actions';
 import { readStreamableValue } from 'ai/rsc';
 
 export default function Chat() {
   const [messages, setMessages] = useState<CoreMessage[]>([]);
   const [input, setInput] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
+
+  function submitMessages() {
+    const newMessages: CoreMessage[] = [
+      ...messages,
+      { content: input, role: 'user' },
+    ];
+    setMessages(newMessages);
+    setInput('');
+    setIsFetching(true);
+  }
+  useEffect(() => {
+    if (!isFetching) return;
+    async function startConversation() {
+      const result = await continueConversation(messages);
+      for await (const content of readStreamableValue(result)) {
+        setMessages([
+          ...messages,
+          {
+            role: 'assistant',
+            content: content as string,
+          },
+        ]);
+      }
+      setIsFetching(false);
+    }
+    startConversation();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isFetching])
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m, i) => (
@@ -18,27 +47,7 @@ export default function Chat() {
       ))}
 
       <form
-        action={async () => {
-          const newMessages: CoreMessage[] = [
-            ...messages,
-            { content: input, role: 'user' },
-          ];
-
-          setMessages(newMessages);
-          setInput('');
-
-          const result = await continueConversation(newMessages);
-
-          for await (const content of readStreamableValue(result)) {
-            setMessages([
-              ...newMessages,
-              {
-                role: 'assistant',
-                content: content as string,
-              },
-            ]);
-          }
-        }}
+        action={submitMessages}
       >
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl dark:text-black"
